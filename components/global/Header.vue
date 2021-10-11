cb7be594838b3c4775350a0072d679f83a820614
Brandon Dith-Berry <brandondithberry@gmail.com>
2021-10-11 11:18:59 -0500

    font

--- components/global/Header.vue
+++ components/global/Header.vue
@@ -1,14 +1,14 @@
 <template>
   <nav class="scrim-bg fixed z-40 top-0 inset-x-0 pt-3 px-3" aria-label="Main Menu">
     <ul class="flex">
-      <li class="flex-1">
-        <nuxt-link class="btn" to="/">Home</nuxt-link>
+      <li>
+        <nuxt-link to="/">Home</nuxt-link>
       </li>
-      <li class="flex-1 ml-2">
-        <nuxt-link class="btn" to="/blog">Blog</nuxt-link>
+      <li>
+        <nuxt-link to="/blog">Blog</nuxt-link>
       </li>
-      <li class="flex-1 ml-2">
-        <nuxt-link class="btn" to="/projects">Projects</nuxt-link>
+      <li>
+        <nuxt-link to="/projects">Projects</nuxt-link>
       </li>
     </ul>
   </nav>
