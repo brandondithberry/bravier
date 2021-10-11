d19704ce443bf4e255e2c721506bfe7b9010371c
Brandon Dith-Berry <brandondithberry@gmail.com>
2021-10-10 23:16:28 -0500

    Updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,11 +40,11 @@ export default {
       {
         rel: 'preload',
         as: 'style',
-        href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap'
+        href: 'https://fonts.googleapis.com/css2?family=Lexend:wght@400;600;700&display=swap'
       },
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap',
+        href: 'https://fonts.googleapis.com/css2?family=Lexend:wght@400;600;700&display=swap',
         media: 'print',
         onload: `this.media='all'`
       }
@@ -52,7 +52,7 @@ export default {
     noscript: [
       {
         innerHTML:
-          '<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap">'
+          '<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lexend:wght@400;600;700&display=swap">'
       }
     ],
     __dangerouslyDisableSanitizers: ['noscript']
@@ -96,7 +96,7 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend(config, ctx) { }
   },
   /*
    ** Custom additions configuration
